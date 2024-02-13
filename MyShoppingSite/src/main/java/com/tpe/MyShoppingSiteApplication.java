c730134610b3d41d54cd7666e9b4c66a5ca30a6e
husnu93 <149675198+husnu93@users.noreply.github.com>
2024-02-13 17:57:12 +0300

    first commit

---
+++ MyShoppingSite/src/main/java/com/tpe/MyShoppingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShoppingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShoppingSiteApplication.class, args);
+	}
+
+}
