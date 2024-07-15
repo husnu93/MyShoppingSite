0aa5b8bde265a90b721c81537e9cf6372db01fea
husnu93 <149675198+husnu93@users.noreply.github.com>
2024-07-15 22:08:51 +0300

    Update Messages.java

--- MyShoppingSite/src/main/java/com/myproject/contactmessages/messages/Messages.java
+++ MyShoppingSite/src/main/java/com/myproject/contactmessages/messages/Messages.java
@@ -2,6 +2,7 @@ package com.myproject.contactmessages.messages;
 
 public class Messages {
     public static final String NOT_FOUND_MESSAGE =  "Message Not Found";
+    
     public static final String CONTACT_MESSAGE_DELETED_SUCCESSFULLY = "Contact Message Deleted Successsfully ";
 
     public static final String WRONG_DATE_MESSAGE = "Wrong Date Fromat";
