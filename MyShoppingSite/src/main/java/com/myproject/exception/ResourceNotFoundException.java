1aa7d2756a243d08c07751b6d44c3d0b088cb365
husnu93 <149675198+husnu93@users.noreply.github.com>
2025-01-01 15:50:58 +0300

    Update ResourceNotFoundException.java

--- MyShoppingSite/src/main/java/com/myproject/exception/ResourceNotFoundException.java
+++ MyShoppingSite/src/main/java/com/myproject/exception/ResourceNotFoundException.java
@@ -3,9 +3,10 @@ package com.myproject.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND) // 404weasd
+@ResponseStatus(HttpStatus.NOT_FOUND) // 404weasduuyt
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message) {
         super(message);
     }
+
 }
